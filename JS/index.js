36fbb5ce18a0d36aebe8029d55a5f6ef6a83dfdf
OwerAndres <owerandres611@gmail.com>
2024-04-02 21:30:18 +0200

    Cuadro de sesion

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,22 @@
+//Funcion cuadro inicio de sesion
+function boxSessionRemoveShow() {
+    event.stopPropagation();
+    var sesion = document.getElementById("sesion-show");
+    sesion.classList.remove("sesion-show");
+}
+function boxSessionAddShow(event) {
+    if (!event.target.matches('input')) {
+        var sesion = document.getElementById("sesion-show");
+        sesion.classList.add("sesion-show");
+    }
+}
+
 //Clone de infiniti slider
 var copy = document.querySelector(".logos_slider").cloneNode(true)
 document.querySelector(".logos").appendChild(copy)
+
+//Cuadro inicio de sesion
+var initSesion = document.getElementById("initSesion");
+initSesion.addEventListener("click",boxSessionRemoveShow);
+window.addEventListener("click",boxSessionAddShow);
+
